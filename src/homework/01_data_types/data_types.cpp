6e142282a54e32d6cae31194c92a8f3555e4a814
csupapo <christopher.supapo@g.austincc.edu>
2021-09-10 18:43:09 -0500

    Revisions - Comments & Proper Grammar

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -1,12 +1,6 @@
 #include "data_types.h"
 
-//example
-/*int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}*/
-
-//write function code here
+//This function will be used to multiply 5 against values entered 
 int multiply_numbers(int num1)
 {
 	int num2 = 5;
--- src/homework/01_data_types/data_types.h
+++ src/homework/01_data_types/data_types.h
@@ -1,7 +1,4 @@
-﻿//example
-//int add_numbers(int num1, int num2);
-
-//write function prototype here
+﻿//This is a function prototype for a function that will be written in data_types.cpp
 int multiply_numbers(int num1);
 
 
