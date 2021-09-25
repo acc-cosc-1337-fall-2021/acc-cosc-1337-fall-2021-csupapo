2ef7f31c2b14366af7fcfe3f796cceacb79c174f
csupapo <christopher.supapo@g.austincc.edu>
2021-09-24 23:13:41 -0500

    homework3

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,26 @@
 //write include statements
+#include "decisions.h"
+#include<iostream>
 
+//write namespaces
+using std::string; 
+using std::cout; 
+using std::cin;
+
+//Write Function code
 int main() 
 {
-	return 0;
+	//Create variables
+	string letter_grade;
+	int grade;
+	int option;
+
+	cout << "Main Menu" << "\n" << "Select an Option" << "\n";
+	cout << "1 - Letter grade using if" << "\n";
+	cout << "2 - Letter grade using switch" << "\n";
+	cout << "3 - Exit" << "\n";
+	cin >> option;
+	
+	// Call menu function
+	letter_grade = menu(option);
 }
