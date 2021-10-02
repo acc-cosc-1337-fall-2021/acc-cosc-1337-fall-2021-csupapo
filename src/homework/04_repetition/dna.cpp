af694a3bc5656843b8c5823c8de2a7748d869d2c
csupapo <christopher.supapo@g.austincc.edu>
2021-10-01 23:52:45 -0500

    homework4

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,47 @@
 //add include statements
+#include<iostream>
+#include "dna.h"
+#include<string>
+
+using std::cout;
+using std::cin;
+
+
 
 //add function(s) code here
+void menu()
+{
+    cout<<"[1] Factorial \n";
+    cout<<"[2] Greatest Common Factor (GCD) \n";
+    cout<<"[3] Exit \n\n";
+    cout<<"Please Select an Option\n";
+}
+
+int factorial(int num)
+{
+    int sum = 1;
+    while (num > 0)
+    {
+        sum = sum * num;
+        num --; 
+    }
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    while(num1 == num2)
+    {
+        if (num1 > num2)
+        {
+            int store_value = num2;
+            num2 = num1;
+            num1 = store_value;
+        }
+        if (num1 > num2)
+        {
+            num1 = num1 - num2;
+        }
+        return num1;
+    }
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,16 +1,50 @@
 //write include statements
-
+#include<iostream>
+#include "dna.h"
 //write using statements
+using std::cout;
+using std::cin;
+using std::string;
+
+//initialize variables
+int option;
+int num;
+int num1, num2;
+int gcd_answer;
 
 
-/*
-Write code that prompts user to enter 1 for Get GC Content, 
-or 2 for Get DNA Complement.  The program will prompt user for a 
-DNA string and call either get gc content or get dna complement
-function and display the result. Program runs as long as 
-user enters a y or Y.
-*/
 int main() 
 {
-	return 0;
+	string run = "no";
+	menu();
+	cin>>option;
+	do
+	{
+		if (option == 1)
+	{
+		cout<<"Please Enter a Number\n";
+		cin>>num;
+		num = factorial(num);
+		cout<<"The answer is " << num;
+		return num;
+	}
+	if (option == 2)
+	{
+		cout<<"Please Enter First Value\n";
+		cin>>num1;
+		cout<<"Please Enter Second Value\n";
+		cin>>num2;
+		gcd_answer = gcd(num1, num2);
+		cout<<"Your GCD is " << gcd_answer;
+		return gcd_answer;
+	}
+	if (option == 3)
+	{
+		cout<<"Are you sure you want to exit?";
+		cin>>run;
+	}
+	
+	}
+	 while (run == "no");
+	
 }
